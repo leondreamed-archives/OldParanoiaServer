662efdcc2fbc3ad0933a26b7ed97bf3fa1d13c7a
Leon Si <leonzalion@gmail.com>
2020-04-12 22:17:29 -0400

    added

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const app = require('express')();
 const http = require('http').createServer(app);
 const port = process.env.PORT || 3000;
 const path = require('path');
+const io = require('socket.io')(80);
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'index.html'));
