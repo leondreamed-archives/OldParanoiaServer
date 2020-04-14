cb01139c072393dd194b55dc9e186204331caadb
Leon Si <leonzalion@gmail.com>
2020-04-13 23:22:07 -0400

    updates

--- app.js
+++ app.js
@@ -4,15 +4,26 @@ const path = require('path');
 const app = require('express')();
 const server = require('http').createServer(app);
 server.listen(port);
-
 const io = require('socket.io').listen(server);
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+let isClientConnected = false;
+
 io.on('connection', function(socket) {
-  console.log('connection');
+  socket.on('get-client-connection-status', function(fn) {
+    fn(isClientConnected);
+  });
+
+  socket.on('client-disconnected', function() {
+    socket.broadcast.emit('client-disconnected');
+  });
+
+  socket.on('client-connected', function() {
+    socket.broadcast.emit('client-connected');
+  })
 
   socket.on('took-screenshot', function(result) {
     socket.broadcast.emit('took-screenshot', result);
