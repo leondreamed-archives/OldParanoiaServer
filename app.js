3c2c5b77a32512c50d45c8e9f65d739fbb7b8d62
Leon Si <leonzalion@gmail.com>
2020-04-13 22:25:35 -0400

    hi

--- app.js
+++ app.js
@@ -4,14 +4,15 @@ const path = require('path');
 const app = require('express')();
 const server = require('http').createServer(app);
 server.listen(port);
-const io = require('socket.io').listen(server);
 
+const io = require('socket.io').listen(server);
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 io.on('connection', function(socket) {
+  console.log('connection');
   socket.on('took-screenshot', function(result) {
     socket.broadcast.emit('took-screenshot', result);
   })
